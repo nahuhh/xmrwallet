f48d3ecf0d033d4ad9e8b217ba234e4044947923
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-07-25 13:45:14 -0400

    Update LoginActivity.java

--- app/src/main/java/com/m2049r/xmrwallet/LoginActivity.java
+++ app/src/main/java/com/m2049r/xmrwallet/LoginActivity.java
@@ -1469,4 +1469,26 @@ public class LoginActivity extends BaseActivity
             waitingUiTask = uiTask;
         }
     }
+
+    public boolean haveNetworkConnection() {
+        try {
+            boolean haveConnectedWifi = false;
+            boolean haveConnectedMobile = false;
+
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+            for (NetworkInfo ni : netInfo) {
+                if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                    if (ni.isConnected())
+                        haveConnectedWifi = true;
+                if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                    if (ni.isConnected())
+                        haveConnectedMobile = true;
+            }
+            return haveConnectedWifi || haveConnectedMobile;
+        } catch(Exception e) {
+            Timber.d("exception in haveNetworkConnection");
+            return false;
+        }
+    }
 }
